a130df4d84ac8b305c68d18d759404423cdf06e8
Anton <a.tashchilin@bk.ru>
2023-12-11 21:41:43 +0400

    change docker-comp kafka and main class

--- src/main/java/com/example/kafkaLearn/firstExamples/KafkaLearnApplication.java
+++ src/main/java/com/example/kafkaLearn/firstExamples/KafkaLearnApplication.java
@@ -1,7 +1,10 @@
 package com.example.kafkaLearn.firstExamples;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.SpringApplication;
@@ -13,27 +16,37 @@ public class KafkaLearnApplication {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
 		SpringApplication.run(KafkaLearnApplication.class, args);
-		var producer = new MyProducer("spring-topic-demo");
-		new Thread(() -> {
-			for (int i = 0; i < 100; i++) {
-				try{
-					producer.send(String.valueOf(i), "Hello from MyProducer!");
-					TimeUnit.SECONDS.sleep(5);
-				}
-				catch(Throwable t){
-					Thread.currentThread().interrupt();
-				}
-			}
-		}).start();
+		var producer = new MyProducer("spring-kafka-demo");
+		var messages = 10000;
+		ExecutorService ex = Executors.newFixedThreadPool(10);
+		CountDownLatch latch = new CountDownLatch(messages);
 
-		var comsumer = new MyConsumer("spring-topic-demo");
-
-		comsumer.consume((r) -> System.out.println("key="+r.key()+", "+"value="+r.value()) );
-
-		TimeUnit.MINUTES.sleep(5);
 		
+		for (int i = 0; i < messages; i++) {
+			final int j = i;
+			ex.submit(() -> extracted(producer, latch, j));
+		}
+		latch.await();
+
+		var comsumer = new MyConsumer("spring-kafka-demo");
+
+		comsumer.consume((r) -> System.out.println("key=" + r.key() + ", "
+		+"value="+r.value() + " partion="+ r.partition()) );
+
+		
+		TimeUnit.MINUTES.sleep(5);
 		producer.close();
 		comsumer.close();
+	}
+
+	private static void extracted(MyProducer producer, CountDownLatch latch, int i) {
+		try{
+			producer.send(String.valueOf(i), "Hello from MyProducer!");
+			latch.countDown();
+		}
+		catch(Throwable t){
+			Thread.currentThread().interrupt();
+		}
 	}
 
 }
