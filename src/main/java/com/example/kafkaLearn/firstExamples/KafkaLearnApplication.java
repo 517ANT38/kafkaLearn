565c72c89418874c50f6599ae30e6679c992136b
Anton <a.tashchilin@bk.ru>
2023-12-13 06:26:02 +0400

    delete instrution unnecessary

--- src/main/java/com/example/kafkaLearn/firstExamples/KafkaLearnApplication.java
+++ src/main/java/com/example/kafkaLearn/firstExamples/KafkaLearnApplication.java
@@ -13,57 +13,19 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-// @SpringBootApplication
+
+@SpringBootApplication
+@EnableAdminServer
 public class KafkaLearnApplication {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
-		// SpringApplication.run(KafkaLearnApplication.class, args);
-		var producer = new MyProducer("spring-topic-demo");
-		var messages = 10000;
-		ExecutorService ex = Executors.newFixedThreadPool(10);
-		CountDownLatch latch = new CountDownLatch(messages);
-
+		SpringApplication.run(KafkaLearnApplication.class, args);
 		
-		for (int i = 0; i < messages; i++) {
-			final int j = i;
-			ex.submit(() -> extracted(producer, latch, j));
-		}
-		latch.await();
-
-		var comsumer = new MyConsumer("spring-topic-demo");
-
-		FileWriter out = new FileWriter("test");
-		
-		comsumer.consume((r) -> {
-			try {
-				out.write("key=" + r.key() + ", "
-				+"value="+r.value() + " partion="+ r.partition() +"\n");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} );
-
-
-		
-		producer.close();
-		ex.shutdown();
-		TimeUnit.MINUTES.sleep(5);
-		comsumer.close();
-
-		out.close();
 		
 	}
 
-	private static void extracted(MyProducer producer, CountDownLatch latch, int i) {
-		try{
-			producer.send(String.valueOf(i), "Hello from MyProducer!");
-			latch.countDown();
-		}
-		catch(Throwable t){
-			Thread.currentThread().interrupt();
-		}
-	}
+	
 
 }
